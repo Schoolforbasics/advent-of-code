b5f4b2624667d462b435a156759bc3b772c92d57
Nikolai Vavilov <'vvnicholas@gmail.com'>
2015-12-08 18:36:30 +0200

    Add Part 2 solution

--- Day 1/day1.cpp
+++ Day 1/day1.cpp
@@ -8,7 +8,13 @@ using namespace std;
 int main() {
 	ifstream f("input");
 
-	cout << accumulate(istream_iterator<char>(f), {}, 0, [](auto sum, auto c) {
-		return sum + (c == '(') - (c == ')');
+	cout << accumulate(istream_iterator<char>(f), {}, 0, [pos = 1, found = false](auto sum, auto c) mutable {
+		sum += (c == '(') - (c == ')');
+		if (sum == -1 && !found) {
+			found = true;
+			cout << pos << endl;
+		}
+		pos++;
+		return sum;
 	}) << endl;
 }
